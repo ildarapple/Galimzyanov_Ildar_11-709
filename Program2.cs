d103e9e76925f57917750eecf1182841f33e1829
ildarapple <31629371+ildarapple@users.noreply.github.com>
2017-09-13 20:49:53 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            a = a % 10 * 100 + a / 10 % 10 * 10 + a / 100;
+            Console.Write(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1 || a == 11 || a == 13 || a == 23) Console.WriteLine("30");
+            else
+            if (a == 2 || a == 10 || a == 14 || a == 22) Console.WriteLine("60");
+            else
+            if (a == 3 || a == 9 || a == 15 || a == 21) Console.WriteLine("90");
+            else
+            if (a == 4 || a == 8 || a == 16 || a == 20) Console.WriteLine("120");
+            else
+            if (a == 5 || a == 7 || a == 17 || a == 19) Console.WriteLine("150");
+            else
+            if (a == 6 || a == 18) Console.WriteLine("180");
+            else
+            if (a == 12 || a == 24) Console.WriteLine("0");
+            if (a == 1 || a == 11) Console.WriteLine("30");
+            Console.ReadLine();
+        }
+    }
+}

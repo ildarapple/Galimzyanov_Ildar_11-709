b5172f221abc7fad9c41be9524cfc9e53fa1c581
ildarapple <31629371+ildarapple@users.noreply.github.com>
2017-09-22 12:29:35 +0300

    Add files via upload

---
+++ Program3_21_09.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            if (x * x - y * y >= 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}

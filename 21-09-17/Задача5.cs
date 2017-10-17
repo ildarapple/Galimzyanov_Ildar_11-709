a2aa7a4074359f3d2d51c6b8b08bee69dc5da790
ildarapple <31629371+ildarapple@users.noreply.github.com>
2017-10-17 11:13:18 +0300

    1

---
+++ 21-09-17/Задача5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i<=1000000; i++)
+            {
+                int s = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0) s += 1;
+                }
+                if (s == 2) Console.WriteLine(i);
+            }
+        }
+    }
+}

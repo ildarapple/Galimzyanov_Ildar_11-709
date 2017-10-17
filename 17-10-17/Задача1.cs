4bb83297e24f37066de31614111a8423703b26f5
ildarapple <31629371+ildarapple@users.noreply.github.com>
2017-10-17 11:11:46 +0300

    1

---
+++ 17-10-17/Задача1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int j = 0;
+            bool yea = false;
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (a[0] == b[i])
+                {
+                    int m = 0;
+                    for (j = i; j < b.Length; j++)
+                    {
+                        if (b[j] == a[m])
+                        {
+                            m++;
+                            if (m == a.Length)
+                            {
+                                yea = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(yea);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 17-10-17/Задача2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5};
+            int min = array[0];
+            int max = array[0];
+            int imin = 0;
+            int imax = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    imin = i;
+                }
+
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    imax = i;
+                }
+            }
+            int smax = 0;
+            int smin = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i != imin)
+                {
+                    smax += array[i];
+                }
+
+                if (i != imax)
+                {
+                    smin += array[i];
+                }
+            }
+            Console.WriteLine("max = " + smax);
+            Console.WriteLine("min = " + smin);
+            Console.ReadLine();
+        }
+    }
+}
